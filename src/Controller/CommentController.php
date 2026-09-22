cc51e5371d99031bede883856449beea83dcea28
valentin <valentin@effetb.com>
2026-09-22 12:22:45 +0200

    protection csrf

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -6,6 +6,7 @@ use App\Entity\Comment;
 use App\Repository\CommentRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -15,12 +16,17 @@ final class CommentController extends AbstractController
     #[Route('/commentaires/{id}/supprimer', name: 'app_comment_delete', methods: ['GET'])]
     public function delete(
         string                 $id,
+        Request                $request,
         EntityManagerInterface $entityManager,
         CommentRepository      $commentRepository
     ): Response
     {
         if (null === $comment = $commentRepository->findOneBy(['id' => $id])) {
             throw $this->createNotFoundException();
+        }
+
+        if (!$this->isCsrfTokenValid('comment_delete_' . $comment->getId(), $request->query->get('_token'))) {
+            throw $this->createAccessDeniedException('Jeton CSRF invalide.');
         }
 
         $topic = $comment->getTopic();
